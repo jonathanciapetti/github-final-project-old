9a7a0ee0a9fc373b0802be5d7b44f3501f17614c
Jonathan Ciapetti <jonathanciapetti@gmx.com>
2022-12-09 03:10:04 +0100

    Update simple_interest.sh

--- simple_interest.sh
+++ simple_interest.sh
@@ -1,1 +1,12 @@
 
+#!/bin/bash
+
+echo " Enter the principle value: "
+read p
+echo " Enter the rate of interest:"
+read r
+echo " Enter the time period:"
+read t
+s=`expr $p \* $t \* $r / 100`
+echo " The simple interest is "
+echo $s
